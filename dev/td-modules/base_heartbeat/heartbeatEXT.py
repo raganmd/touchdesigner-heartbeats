82a2b166e6fc8b8f36a81926de10dbdc90b915c5
Matthew Ragan <raganmd@gmail.com>
2019-01-27 02:18:08 -0800

    Working Pieces
    Functional elements started
    Initial pieces set-up
    Custom parameters set-up

--- dev/td-modules/base_heartbeat/heartbeatEXT.py
+++ dev/td-modules/base_heartbeat/heartbeatEXT.py
@@ -2,6 +2,7 @@
 Matthew Ragan | matthewragan.com
 '''
 
+import json
 
 class Heartbeat:
 	'''
@@ -16,7 +17,28 @@ class Heartbeat:
  	'''
 
 	def __init__(self, myOp):
+		self.Active 		= parent().par.Active
+		self.HeartbeatRole 	= parent().par.Heartbeatrole
+		self.Udp_out		= op('udpout1')
+		self.Udp_in 		= op('udpin1')
 		pass
 	
-	def My_fav_method(self, arg1):
+	def Request_hb(self):
+		Controller 			= 1 if self.HeartbeatRole.eval() == 'controller' else 0
+
+		if Controller:
+			msg = {"heart_beat":"hello"}
+			self.Udp_out.sendBytes(str(msg))
+		else:
+			pass
+		pass
+	
+	def Send_hb(self, msg):
+		pass
+
+	def Process_msg(self, msg, debug=False):
+		json_msg = json.dumps(msg)
+		
+		if debug:
+			print(json_msg)
 		pass
